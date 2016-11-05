a56e147948356675a1b820cde5c481ccc1add73a
Victor Cordero Utrilla <utrilla.victor@gmail.com>
2016-11-04 20:24:02 -0400

    Covered: Threads using Grand Central Dispatch (GCD) to fix Project7

---
+++ Project9/Project9/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Project9
+//
+//  Created by Victor Cordero on 2016-11-04.
+//  Copyright © 2016 Victor Cordero Utrilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [[String: String]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+
+    func fetchJSON() {
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    self.parse(json: json)
+                    return
+                }
+            }
+        }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        
+    }
+    
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+            self.tableView.reloadData()
+    }
+
+    func showError() {
+        let ac = UIAlertController(title: "Loading Error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
