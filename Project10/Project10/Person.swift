041e03802066719df9adf73ab406e65b1904d16f
Victor Cordero Utrilla <utrilla.victor@gmail.com>
2016-11-05 21:55:03 -0400

    Covered: UICollectionView, UIImagePickerController, UUID and classes

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Victor Cordero on 2016-11-04.
+//  Copyright © 2016 Victor Cordero Utrilla. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
