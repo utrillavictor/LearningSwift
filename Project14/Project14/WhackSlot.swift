606a23940ad1d8511318f4e1b559b74b5bb57d36
Victor Cordero Utrilla <utrilla.victor@gmail.com>
2016-11-11 21:30:50 -0500

    Covered: Project14 part 1 of whack a penguin

---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,62 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by Victor Cordero Utrilla on 11/11/16.
+//  Copyright © 2016 utrillavictor. All rights reserved.
+//
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
+
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+
+        if RandomInt(min: 0, max: 2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [unowned self] in
+            self.hide()
+        }
+    }
+
+    func hide() {
+        if !isVisible { return }
+
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.085))
+        isVisible = false
+    }
+}
